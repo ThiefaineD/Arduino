ebf8551858ff350fb126cc69b810b78983564613
Thiefaine DOUHET <thiefainedouhet@gmail.com>
2016-11-15 14:35:44 +0100

    Add arduino folder

---
+++ arduino/server/boot/create-model.js
@@ -0,0 +1,18 @@
+/*module.exports = function(app) {
+  app.dataSources.postgres.automigrate('humidity', function(err) {
+    if (err) throw err;
+      console.log('Models created: \n', humidity);
+    });
+  app.dataSources.postgres.automigrate('temperature', function(err) {
+    if (err) throw err;
+      console.log('Models created: \n', temperature);
+    });
+  app.dataSources.postgres.automigrate('alert', function(err) {
+    if (err) throw err;
+      console.log('Models created: \n', alert);
+    });
+  app.dataSources.postgres.automigrate('detection', function(err) {
+    if (err) throw err;
+      console.log('Models created: \n', detection);
+    });
+};*/
